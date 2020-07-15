8bb9e3e7866f4a27cdabc7b11731b0625b2d27c0
amazingchow <jianzhou42@163.com>
2020-07-15 18:07:29 +0800

    upgrade leader election

---
+++ electleader/leader.go
@@ -0,0 +1,129 @@
+package electleader
+
+import (
+	"time"
+
+	leaderelection "github.com/Comcast/go-leaderelection"
+	"github.com/rs/zerolog/log"
+	"github.com/samuel/go-zookeeper/zk"
+)
+
+const (
+	_ElectionPath = "/election"
+	_LeaderPath   = "/leader"
+
+	_ElectionHeartbeat = 10 * time.Second
+)
+
+// Elector 用于选主
+type Elector struct {
+	conn         *zk.Conn
+	participator string
+	cfg          *ElectorCfg
+}
+
+// ElectorCfg 选主配置
+type ElectorCfg struct {
+	ZKEndpoints []string `json:"zk_endpoints"`
+}
+
+// NewElector 返回Elector实例.
+func NewElector(cfg *ElectorCfg) *Elector {
+	return &Elector{
+		cfg: cfg,
+	}
+}
+
+// ElectLeader 让participator参与选主.
+func (e *Elector) ElectLeader(participator string) {
+	conn, sessionEv, err := zk.Connect(e.cfg.ZKEndpoints, _ElectionHeartbeat)
+	if err != nil {
+		log.Fatal().Err(err).Str("[participator]", participator).Msgf("failed to connect to zookeeper cluster (%v)", e.cfg.ZKEndpoints)
+	}
+	e.conn = conn
+	e.participator = participator
+
+	e.prepare()
+
+	candidate, err := leaderelection.NewElection(e.conn, _ElectionPath, e.participator)
+	if err != nil {
+		log.Fatal().Err(err).Str("[participator]", e.participator).Msg("failed to start election for candidate")
+	}
+
+	go candidate.ElectLeader()
+
+ELECT_LOOP:
+	for {
+		select {
+		case ev := <-sessionEv:
+			{
+				if ev.State == zk.StateExpired {
+					log.Error().Err(zk.ErrSessionExpired).Str("[participator]", e.participator)
+					break ELECT_LOOP
+				} else if ev.State == zk.StateAuthFailed {
+					log.Error().Err(zk.ErrAuthFailed).Str("[participator]", e.participator)
+					break ELECT_LOOP
+				} else if ev.State == zk.StateUnknown {
+					log.Error().Err(zk.ErrUnknown).Str("[participator]", e.participator)
+					break ELECT_LOOP
+				} else if ev.State.String() == "unknown state" {
+					break ELECT_LOOP
+				}
+				if ev.State == zk.StateDisconnected {
+					log.Warn().Str("[participator]", e.participator).Msgf("zookeeper server (%s) state turns into %s", ev.Server, ev.State.String())
+				}
+				log.Info().Str("[participator]", e.participator).Msgf("zookeeper server (%s) state turns into %s", ev.Server, ev.State.String())
+			}
+		case status, ok := <-candidate.Status():
+			{
+				if !ok {
+					log.Error().Str("[participator]", e.participator).Msg("election channel has been closed, election will terminate")
+					break ELECT_LOOP
+				} else if status.Err != nil {
+					log.Error().Err(status.Err).Str("[participator]", e.participator).Msg("candidate received election status error")
+					break ELECT_LOOP
+				} else {
+					log.Info().Str("[participator]", e.participator).Msgf("candidate received status message\n\n%v", status.String())
+					if status.Role == leaderelection.Leader {
+						e.setLeader(e.participator)
+						// TODO: do your stuff
+						log.Info().Str("[participator]", e.participator).Msg("candidate has been promoted to leader")
+					} else if status.Role == leaderelection.Follower {
+						e.getLeader()
+						// TODO: do your stuff
+					}
+				}
+			}
+		}
+	}
+	candidate.Resign()
+	e.conn.Close()
+}
+
+func (e *Elector) prepare() {
+	_, err := e.conn.Create(_ElectionPath, []byte(""), 0, zk.WorldACL(zk.PermAll))
+	if err != nil && err != zk.ErrNodeExists {
+		log.Fatal().Err(err).Str("[participator]", e.participator).Msgf("failed to create znode (%s)", _ElectionPath)
+	}
+
+	_, err = e.conn.Create(_LeaderPath, []byte(""), 0, zk.WorldACL(zk.PermAll))
+	if err != nil && err != zk.ErrNodeExists {
+		log.Fatal().Err(err).Str("[participator]", e.participator).Msgf("failed to create znode (%s)", _ElectionPath)
+	}
+}
+
+func (e *Elector) setLeader(leader string) {
+	_, err := e.conn.Set(_LeaderPath, []byte(leader), -1)
+	if err != nil {
+		log.Fatal().Err(err).Str("[participator]", e.participator).Msgf("failed to set value for znode (%s)", _LeaderPath)
+	}
+}
+
+func (e *Elector) getLeader() string {
+	v, _, err := e.conn.Get(_LeaderPath)
+	if err != nil {
+		log.Fatal().Err(err).Str("[participator]", e.participator).Msgf("failed to get value from znode (%s)", _LeaderPath)
+	}
+
+	return string(v)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/Comcast/go-leaderelection v0.0.0-20181102191523-272fd9e2bddc
+	github.com/rs/zerolog v1.19.0
 	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da
 	github.com/stretchr/testify v1.5.1
 )
