ee65aed1fc3fb8020994fc9ee156694396431340
amazingchow <jianzhou42@163.com>
2020-10-29 23:53:02 +0800

    修改ExtMutex

--- extsync/extmutex.go
+++ extsync/extmutex.go
@@ -13,42 +13,46 @@ const (
 	mutexWaiterShift = iota
 )
 
-// ExtMutex 互斥锁的拓展版本
-type ExtMutex struct {
+// Mutex 高级互斥锁
+type Mutex struct {
 	sync.Mutex
 }
 
-// TryLock ...
-func (m *ExtMutex) TryLock() bool {
+// TryLock 尝试获取锁(非阻塞).
+func (m *Mutex) TryLock() bool {
 	if atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&m.Mutex)), 0, mutexLocked) {
+		// fast path
 		return true
 	}
-
 	old := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
 	if old&(mutexLocked|mutexWoken|mutexStarving) != 0 {
 		return false
 	}
-
 	new := old | mutexLocked
 	return atomic.CompareAndSwapInt32((*int32)(unsafe.Pointer(&m.Mutex)), old, new)
 }
 
-// Count ...
-func (m *ExtMutex) Count() int {
+// Count 统计当前等待获取锁的协程数.
+func (m *Mutex) Count() int {
 	v := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
-	v = v >> mutexWaiterShift
-	v = v + (v & mutexLocked)
+	v = v>>mutexWaiterShift + (v & mutexLocked)
 	return int(v)
 }
 
-// IsWoken ...
-func (m *ExtMutex) IsWoken() bool {
+// IsLocked 判断锁当前是否处于被持有状态.
+func (m *Mutex) IsLocked() bool {
+	v := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
+	return v&mutexLocked == mutexLocked
+}
+
+// IsWoken 判断锁当前是否处于被唤醒状态.
+func (m *Mutex) IsWoken() bool {
 	v := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
 	return v&mutexWoken == mutexWoken
 }
 
-// IsStarving ...
-func (m *ExtMutex) IsStarving() bool {
+// IsStarving 判断锁当前是否处于饥饿状态.
+func (m *Mutex) IsStarving() bool {
 	v := atomic.LoadInt32((*int32)(unsafe.Pointer(&m.Mutex)))
 	return v&mutexStarving == mutexStarving
 }
