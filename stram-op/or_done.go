e1d6f3c843db7b73b3501d98130c992816f14c06
amazingchow <zhoujian2@sensetime.com>
2020-12-24 18:02:29 +0800

    update or-done stream op

--- stram-op/or_done.go
+++ stram-op/or_done.go
@@ -5,23 +5,23 @@ import (
 )
 
 // OrDone 一种信号通知模式,
-// 如果有多个同质微服务任务要执行, 只要其中任意一个任务执行完毕, 就通知上游/下游服务.
-func OrDone(notifyChans ...<-chan interface{}) <-chan interface{} {
+// 如果有多个同类微服务任务同时执行, 只要其中一个任务执行完毕, 就通知上游/下游服务.
+func OrDone(notifyChs ...chan struct{}) chan struct{} {
 	// 特殊情况, 只有0个或者1个微服务
-	switch len(notifyChans) {
+	switch len(notifyChs) {
 	case 0:
 		return nil
 	case 1:
-		return notifyChans[0]
+		return notifyChs[0]
 	}
 
-	orDone := make(chan interface{})
+	orDone := make(chan struct{})
 	go func() {
 		defer close(orDone)
 
 		// 利用反射构建SelectCase
 		var cases []reflect.SelectCase
-		for _, ch := range notifyChans {
+		for _, ch := range notifyChs {
 			cases = append(cases, reflect.SelectCase{
 				Dir:  reflect.SelectRecv,
 				Chan: reflect.ValueOf(ch),
